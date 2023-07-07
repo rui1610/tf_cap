506aaaa55054560a7984a721f218f0439e13926e
Rui Nogueira <rui.nogueira@sap.com>
2023-07-07 21:10:35 +0200

    dd cf provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,3 +10,8 @@ terraform {
 provider "btp" {
   globalaccount = "ticrossa"
 }
+
+// Configuration is described in https://registry.terraform.io/providers/cloudfoundry-community/cloudfoundry/latest/docs
+provider "cloudfoundry" {
+  api_url  = "https://api.cf.us10.hana.ondemand.com"
+}
