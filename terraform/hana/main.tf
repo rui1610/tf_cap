fefbfe02ef88ea84e3128e434196c74ef9441ed2
Rui Nogueira <rui.nogueira@sap.com>
2023-07-12 19:25:43 +0000

    fix typo for hana data resource usage

--- terraform/hana/main.tf
+++ terraform/hana/main.tf
@@ -5,5 +5,5 @@ data "cloudfoundry_service" "hana" {
 resource "cloudfoundry_service_instance" "hana_hdi_shared" {
   name         = "hana"
   space        = var.cf_space_id
-  service_plan = data.cloudfoundry_service.hana_cloud.service_plans["hdi-shared"]
+  service_plan = data.cloudfoundry_service.hana.service_plans["hdi-shared"]
 }
