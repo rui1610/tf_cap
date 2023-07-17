057d4adfce1e20b2f57f6047f85040994fecf9b9
Rui Nogueira <rui.nogueira@sap.com>
2023-07-17 16:29:05 +0200

    prep structure

--- terraform/hana/variables.tf
+++ terraform/hana/variables.tf
@@ -1,4 +1,44 @@
-variable "cf_space_id" {
+variable "region" {
   type        = string
-  description = "The technical ID of the Cloud Foundry space."
+  description = "Defines to which organisation the sub account shall belong to."
+  default     = "us10"
+}
+
+variable "subaccount_name" {
+  type        = string
+  description = "Defines the name of the sub account."
+  default     = "Demo CAP app"
+}
+
+variable "app_admins" {
+  type        = list(string)
+  description = "Defines the administrators for the sub account and app."
+  default     = ["rui.nogueira@sap.com","jeremias.roessler@sap.com"]
+}
+
+variable "repo_name" {
+  type        = string
+  description = "Defines the GitHub repository name for the sub account"
+  default     = "temp"
+}
+
+variable "entitlements" {
+  type = list(object({
+      service_name = string
+      plan_name    = string
+  }))
+  description = "List of entitlements for a BTP subaccount"
+
+  default = [
+    {
+      service_name = "hana-cloud"
+      plan_name    = "hana"
+    },
+    {
+    service_name = "hana"
+     plan_name    = "hdi-shared"
+    }
+  ]
+
+
 }
