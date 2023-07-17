77630d9fa715aa65dfd5275fdc83123feda85f45
Rui Nogueira <rui.nogueira@sap.com>
2023-07-17 08:30:33 +0200

    update whitetlist IP list

--- terraform/hana_cloud/main.tf
+++ terraform/hana_cloud/main.tf
@@ -6,5 +6,5 @@ resource "cloudfoundry_service_instance" "hana_cloud_hana" {
   name         = "hana-cloud"
   space        = var.cf_space_id
   service_plan = data.cloudfoundry_service.hana_cloud.service_plans["hana"]
-  json_params  = jsonencode({"data": {"memory": 30,"edition": "cloud","systempassword": "Abcd1234", "whitelistIPs":["0.0.0.0/24"]}})
+  json_params  = jsonencode({"data": {"memory": 30,"edition": "cloud","systempassword": "Abcd1234", "whitelistIPs": ["0.0.0.0/0"]}})
 }
